ee0929c14539102e5ed42424fa5c51c2dcb6843d
Gilbert Luis <gilbertluis97@gmail.com>
2024-11-19 15:24:12 -0500

    make a seeder for default user example

---
+++ Modules/Auth/database/seeders/SeedDefaultAdminUserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Auth\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Auth\Models\User;
+
+class SeedDefaultAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name'     => 'Admin',
+            'email'    => 'admin@example.org',
+            'password' => bcrypt('1234'),
+        ]);
+    }
+}
