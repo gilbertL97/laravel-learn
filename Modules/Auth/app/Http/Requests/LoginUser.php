50f2eab616fe03a60e68b7a5fa4f73fedfb9ae9b
Gilbert Luis <gilbertluis97@gmail.com>
2024-11-24 22:20:50 -0500

    creating form request for login

---
+++ Modules/Auth/app/Http/Requests/LoginUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Auth\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginUser extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required | min:4 | max:25',
+            'password' => 'required  | min:6 | max:100'
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
