07b1c412b9acdf60c114fa2f805e29910ed5ad32
Gilbert Luis <gilbertluis97@gmail.com>
2024-11-26 03:36:49 -0500

    change retorning error to aplication json

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+            $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+                if ($request->is('*')) {
+                    return true;
+                }
+     
+                return $request->expectsJson();
+            });
+        })->create();
+
