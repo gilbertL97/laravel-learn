8d844c9821a8bf64219fb62c757b89119eb166a7
Gilbert Luis <gilbertluis97@gmail.com>
2025-02-12 15:38:49 -0500

    fix name in media migration

--- Modules/Blog/database/migrations/2025_02_12_202901_create_post_media.php
+++ Modules/Blog/database/migrations/2025_02_12_202901_create_post_media.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('', function (Blueprint $table) {
+        Schema::create('media', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->string('media');
