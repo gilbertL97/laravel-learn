a5a85a217bb3df2a9a353284abec7d9f86c3a5dc
Gilbert Luis <gilbertluis97@gmail.com>
2025-02-12 16:05:02 -0500

    adding metods to controller

--- Modules/Blog/app/Http/Controllers/BlogController.php
+++ Modules/Blog/app/Http/Controllers/BlogController.php
@@ -7,45 +7,22 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return view('blog::index');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('blog::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
-
+    public function showAll($id) {}
     /**
      * Show the specified resource.
      */
-    public function show($id)
-    {
-        return view('blog::show');
-    }
+    public function show($id) {}
 
+    public function showPrevNext($id) {}
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit($id)
-    {
-        return view('blog::edit');
-    }
+    public function edit($id) {}
 
     /**
      * Update the specified resource in storage.
