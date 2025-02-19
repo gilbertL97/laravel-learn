68630859a718c2750b6705774ab21aa354b20bc4
Gilbert Luis <gilbertluis97@gmail.com>
2025-02-19 13:28:38 -0500

    make a middleware for permission and roles

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,21 +9,24 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         //web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__ . '/../routes/api.php',
         apiPrefix: 'laravel',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-            $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
-                if ($request->is('*')) {
-                    return true;
-                }
-     
-                return $request->expectsJson();
-            });
-        })->create();
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('*')) {
+                return true;
+            }
 
+            return $request->expectsJson();
+        });
+    })->create();
